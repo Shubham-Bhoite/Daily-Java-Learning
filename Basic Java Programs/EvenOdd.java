401ed042804ea2c710b37b82ada5fc68448d2b60
SHUBHAM BHOITE <shubhambhoite21052002@gmail.com>
2024-03-09 14:35:48 +0530

    add basic programs

---
+++ Basic Java Programs/EvenOdd.java
@@ -0,0 +1,9 @@
+class EvenOddNotUsingMod
+{
+	public static void main(String[] args)
+	{
+		int num = 73;
+		// System.out.println(((num/2)*2==num)? "Even" : "Odd");
+		System.out.println(((num/2.0)==(num/2))? "Even" : "Odd");
+	}
+}
