7668559526f70ce8c0c43e744b07401de0b337c8
SHUBHAM BHOITE <shubhambhoite21052002@gmail.com>
2024-05-13 18:49:12 +0530

    add some examples of polymorphism

---
+++ Polymorphism/Method Overloading/Addition.java
@@ -0,0 +1,18 @@
+class Addition{
+	public void add(int a, int b)
+	{
+		System.out.println(a+b);
+	}
+	public void add(int a, int b, int c)
+	{
+		System.out.println(a+b+c);
+	}
+	public void add(int a, int b, int c, int d)
+	{
+		System.out.println(a+b+c+d);
+	}
+
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ Polymorphism/Method Overloading/MethodOverloading1.java
@@ -0,0 +1,27 @@
+class Ramesh
+{
+	public void greeting()
+	{
+		System.out.println("GOOD MORNING");
+	}
+}
+
+class Suresh extends Ramesh
+{
+	public void m1()
+	{
+		System.out.println("HELLO");
+	}
+	public void greeting(String name)
+	{
+		System.out.println("GOOD AFTERNOON");
+	}
+
+}
+
+class MethodOverloading1
+{
+	public static void main(String[] args) {
+		
+	}
+}
