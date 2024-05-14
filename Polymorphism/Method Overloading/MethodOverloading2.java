1bdfd1e636dd1455d3a6297274a7e1df2100ce64
SHUBHAM BHOITE <shubhambhoite21052002@gmail.com>
2024-05-14 11:16:06 +0530

    add 1 example

---
+++ Polymorphism/Method Overloading/MethodOverloading2.java
@@ -0,0 +1,15 @@
+// we can also overload a main method.
+class MethodOverloading2
+{
+	public static void main(String[] args) {
+		System.out.println("BYEE");
+	}
+
+	public static void main() {
+		System.out.println("HELLO");
+	}
+
+	public static void main(int a) {
+		System.out.println("HIII");
+	}
+}
