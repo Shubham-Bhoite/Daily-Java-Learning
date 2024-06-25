d72959d5f9d2373ac9463cb53f617e541de863b5
SHUBHAM BHOITE <shubhambhoite21052002@gmail.com>
2024-06-25 23:09:48 +0530

    just started learning Arrays and declaration of arrays

---
+++ Arrays/ArrayDeclaration.java
@@ -0,0 +1,16 @@
+Class ArrayDeclaration
+{
+	public static void main(String[] args) {
+		//Declare arrays in multiple ways ;
+		int []a; //1d
+		int b[]; //1d
+		byte []c; //1d
+		short[][]d; //2d
+		int[] e,f,g; //1d
+		int [][] h,l; //2d
+		int [] j[],k; //j-2d k-1d
+		int [] i[], m[][]; //i-2d m-3d
+		int []n, []o,p; //CTE
+		int a[], b;
+ 	}
+}
