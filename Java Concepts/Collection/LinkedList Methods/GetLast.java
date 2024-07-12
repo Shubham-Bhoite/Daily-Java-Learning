8c0bfeedcd8232118b97df999db224aff0932343
SHUBHAM BHOITE <shubhambhoite21052002@gmail.com>
2024-07-12 11:23:02 +0530

    add LinkedList Methods examples

---
+++ Java Concepts/Collection/LinkedList Methods/GetLast.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class GetLast{
+
+	public static void main(String[] args) {
+		LinkedList list = new LinkedList();
+		list.add(10);
+		list.add(20);
+		list.add(30);
+		list.add(40);
+
+		System.out.println(list.getLast());
+		
+
+	}
+}
---
+++ Java Concepts/Collection/LinkedList Methods/RemoveLast.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class RemoveLast{
+
+	public static void main(String[] args) {
+		LinkedList list = new LinkedList();
+		list.add(10);
+		list.add(20);
+		list.add(30);
+		list.add(40);
+
+		list.removeLast();
+		System.out.println(list);
+		
+
+	}
+}
