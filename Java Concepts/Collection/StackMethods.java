d4c55c0c8a496311d3249f0243f042e03342257c
SHUBHAM BHOITE <shubhambhoite21052002@gmail.com>
2024-07-18 20:05:09 +0530

    cls

---
+++ Java Concepts/Collection/StackMethods.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class StackSearchMethods
+{
+	public static void main(String[] args) {
+		Stack <Integer> stack = new Stack<>();
+		stack.push(10);
+		stack.push(20);
+		stack.push(30);
+		stack.push(40);
+		System.out.println(stack.search(40));
+
+
+	}
+}
