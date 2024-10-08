7cf67dec94a944fabad21fc4878dee864b1651ad
SHUBHAM BHOITE <shubhambhoite21052002@gmail.com>
2024-10-08 15:03:10 +0530

    add one example

---
+++ Java Concepts/Polymorphism/Method Overriding/SimpleAnimalSounds.java
@@ -0,0 +1,31 @@
+class Animal {
+    void sound() {
+        System.out.println("Some generic animal sound");
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+class Cat extends Animal {
+    @Override
+    void sound() {
+        System.out.println("Cat meows");
+    }
+}
+
+public class SimpleAnimalSounds {
+    public static void main(String[] args) {
+        Animal myAnimal = new Animal();
+        Animal myDog = new Dog();
+        Animal myCat = new Cat();
+
+        myAnimal.sound();  
+        myDog.sound();     
+        myCat.sound();     
+    }
+}
