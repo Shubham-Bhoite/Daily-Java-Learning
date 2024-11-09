92647b717467574542b3a8b3602e45c78876013a
SHUBHAM BHOITE <shubhambhoite21052002@gmail.com>
2024-11-09 19:48:19 +0530

    add SimpleEncryption

---
+++ Mini Projects/SimpleEncryption.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class SimpleEncryption {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter text to encrypt: ");
+        String text = scanner.nextLine();
+
+        System.out.print("Enter shift value (1-25): ");
+        int shift = scanner.nextInt();
+
+        String encryptedText = encrypt(text, shift);
+        System.out.println("Encrypted Text: " + encryptedText);
+
+        String decryptedText = decrypt(encryptedText, shift);
+        System.out.println("Decrypted Text: " + decryptedText);
+
+        scanner.close();
+    }
+
+    public static String encrypt(String text, int shift) {
+        StringBuilder encrypted = new StringBuilder();
+        for (char c : text.toCharArray()) {
+            if (Character.isLetter(c)) {
+                char shiftedChar = (char) (c + shift);
+                if ((Character.isLowerCase(c) && shiftedChar > 'z') || (Character.isUpperCase(c) && shiftedChar > 'Z')) {
+                    shiftedChar -= 26;
+                }
+                encrypted.append(shiftedChar);
+            } else {
+                encrypted.append(c);
+            }
+        }
+        return encrypted.toString();
+    }
+
+    public static String decrypt(String text, int shift) {
+        return encrypt(text, -shift);
+    }
+}
